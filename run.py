29181e6d44d474235fe7e0acc1c5653a6ea913b3
karl <mayuhengkarl@gmail.com>
2025-03-13 22:04:34 +0400

    update

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from scripts.visualization_process import run_base_visualization
 from scripts.sample_size_auxilary_kappa import run_base_auxilary_kappa
 from scripts.sample_size_auxilary_M import run_base_auxilary_M
 from scripts.sample_size_auxilary_gamma import run_base_auxilary_gamma
-# from scripts.real_data import run_base_realdata
 from scripts.order_of_auxiliary import run_base_order_auxiliary
 
 # run_base_simulation()
@@ -15,6 +14,5 @@ run_base_sample_size()
 # run_base_auxilary_kappa()
 # run_base_auxilary_gamma()
 # run_base_auxilary_M()
-# run_base_realdata()
 # run_base_order_auxiliary()
 
